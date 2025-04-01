c4a9522749f6196cbf4bbb29818adaa131a0d377
ENPe5v3ldj1nn <raijlink@gmail.com>
2025-04-01 12:41:05 +0300

    update files

---
+++ Runtime/CoroutineController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+
+public class CoroutineController : MonoBehaviour
+{
+    private static CoroutineController _instance;
+
+    public static CoroutineController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("[CoroutineController]");
+                _instance = go.AddComponent<CoroutineController>();
+                Object.DontDestroyOnLoad(go);
+            }
+
+            return _instance;
+        }
+        private set => _instance = value;
+    }
+}
+
+
+public static class CoroutineExtension
+{
+    public static void Start(this IEnumerator method)
+    {
+        CoroutineController.Instance.StartCoroutine(method);
+    }
+
+    public static void Start(this IEnumerator method, out Coroutine coroutine)
+    {
+        coroutine = CoroutineController.Instance.StartCoroutine(method);
+    }
+
+    public static void Start(this IEnumerator method, out Coroutine coroutine, MonoBehaviour behaviour)
+    {
+        coroutine = behaviour.StartCoroutine(method);
+    }
+
+    public static void Stop(this IEnumerator method)
+    {
+        CoroutineController.Instance.StopCoroutine(method);
+    }
+
+    public static void Stop(this IEnumerator method, Coroutine coroutine)
+    {
+        if (coroutine != null)
+        {
+            CoroutineController.Instance.StopCoroutine(method);
+        }
+    }
+
+    public static void Stop(this IEnumerator method, Coroutine coroutine, MonoBehaviour behaviour)
+    {
+        if (coroutine != null)
+        {
+            behaviour.StopCoroutine(method);
+        }
+    }
+}
