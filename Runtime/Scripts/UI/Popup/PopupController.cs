fd334291fb0db07cfd97a6b0ce9706022a589bac
Dmytro <238030722+UnityRifbar@users.noreply.github.com>
2025-11-26 16:10:18 +0200

    update popups

--- Runtime/Scripts/UI/Popup/PopupController.cs
+++ Runtime/Scripts/UI/Popup/PopupController.cs
@@ -1,22 +1,23 @@
-﻿using enp_unity_extensions.Scripts.UI.Windows;
+﻿using System.Collections.Generic;
+using DG.Tweening;
+using enp_unity_extensions.Scripts.UI.Windows;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
-using DG.Tweening;
 using static enp_unity_extensions.Scripts.UI.Windows.AnimatedWindowConstant;
 
-namespace enp_unity_extensions.Scripts.UI.Popup
+namespace enp_unity_extensions.Runtime.Scripts.UI.Popup
 {
     public class PopupController : MonoBehaviour
     {
         [SerializeField] private Canvas _canvas;
         [SerializeField] private Image _background;
     
-        public static bool PopupIsActive => Instance._activeWindow != null;
+        public static bool PopupIsActive => Instance._windowStack.Count > 0;
         public static float CanvasScale => Instance._canvas.scaleFactor;
     
-        private PopupWindow _activeWindow;
-        private const float AnimSpeed = 0.45f;
+        private readonly Stack<PopupWindow> _windowStack = new Stack<PopupWindow>();
+        private static float AnimSpeed = 0.45f;
         private static readonly string PopupPath = "Popups/";
     
         private static PopupController _instance;
@@ -38,17 +39,20 @@ namespace enp_unity_extensions.Scripts.UI.Popup
             }
         }
 
+        public static void SetAnimSpeed(float animSpeed)
+        {
+            AnimSpeed = animSpeed;
+        }
     
         public static T Open<T>(AnimatedWindowConstant openAnim = OpenMiddle) where T : PopupWindow
         {
             Instance.gameObject.SetActive(true);
             Instance._canvas.gameObject.SetActive(true);
-            Instance._background.DOFade(1, AnimSpeed);
-            
-            if (Instance._activeWindow != null)
+
+            if (Instance._windowStack.Count == 0)
             {
-                Destroy(Instance._activeWindow.gameObject);
-                Instance._activeWindow = null;
+                Instance._background.DOKill();
+                Instance._background.DOFade(1, AnimSpeed);
             }
     
             return SetPopup<T>(typeof(T).Name, openAnim);
@@ -56,24 +60,66 @@ namespace enp_unity_extensions.Scripts.UI.Popup
     
         public static void Close(AnimatedWindowConstant closeAnim = CloseMiddle, UnityAction onClose = null)
         {
+            Close(null, closeAnim, onClose);
+        }
+
+        public static void Close(PopupWindow popup, AnimatedWindowConstant closeAnim = CloseMiddle, UnityAction onClose = null)
+        {
+            if (Instance._windowStack.Count == 0)
+            {
+                onClose?.Invoke();
+                return;
+            }
+
+            var popupToClose = popup ?? Instance._windowStack.Peek();
+
+            if (!Instance._windowStack.Contains(popupToClose))
+            {
+                D.Log("PopupController: popup to close is not registered in the stack.");
+                onClose?.Invoke();
+                return;
+            }
+
+            if (!ReferenceEquals(Instance._windowStack.Peek(), popupToClose))
+            {
+                D.Log("PopupController: only the top-most popup can be closed.");
+                onClose?.Invoke();
+                return;
+            }
+
+            if (Instance._windowStack.Count == 1)
+            {
+                Instance._background.DOKill();
+                Instance._background.DOFade(0, AnimSpeed)
+                    .OnComplete(() => Instance.gameObject.SetActive(false));
+            }
+
             void OnComplete()
             {
-                Destroy(Instance._activeWindow.gameObject);
-                Instance._activeWindow = null;
-                Instance._canvas.gameObject.SetActive(false);
+                Instance._windowStack.Pop();
+                Destroy(popupToClose.gameObject);
+
+                if (Instance._windowStack.Count == 0)
+                {
+                    Instance._canvas.gameObject.SetActive(false);
+                }
+                else
+                {
+                    Instance._windowStack.Peek().transform.SetAsLastSibling();
+                }
+
                 onClose?.Invoke();
             }
     
-            Instance._activeWindow.Close(closeAnim.ToString(), OnComplete);
-            Instance._background.DOFade(0, AnimSpeed)
-            .OnComplete(() => Instance.gameObject.SetActive(false));
+            popupToClose.Close(closeAnim.ToString(), OnComplete);
         }
     
         private static T SetPopup<T>(string name, AnimatedWindowConstant openAnim) where T : PopupWindow
         {
             var popupFromResource = Resources.Load<T>(PopupPath + name);
             var popup = Instantiate(popupFromResource, Instance._canvas.transform);
-            Instance._activeWindow = popup;
+            Instance._windowStack.Push(popup);
+            popup.transform.SetAsLastSibling();
             popup.Open(openAnim.ToString());
             return popup;
         }
--- Runtime/Scripts/UI/Popup/PopupWindow.cs
+++ Runtime/Scripts/UI/Popup/PopupWindow.cs
@@ -1,13 +1,13 @@
 ﻿using enp_unity_extensions.Scripts.UI.Windows;
 using static enp_unity_extensions.Scripts.UI.Windows.AnimatedWindowConstant;
 
-namespace enp_unity_extensions.Scripts.UI.Popup
+namespace enp_unity_extensions.Runtime.Scripts.UI.Popup
 {
     public class PopupWindow : AnimatedWindow
     {
         public void Close(AnimatedWindowConstant closeAnim = CloseMiddle)
         {
-            PopupController.Close(closeAnim);
+            PopupController.Close(this, closeAnim);
         }
     }
 }
