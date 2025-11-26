29514c25b62cf83a07252c297b78b34b8bffb96a
Dmytro <238030722+UnityRifbar@users.noreply.github.com>
2025-11-26 17:41:51 +0200

    add validation

--- Runtime/Scripts/UI/Popup/PopupWindow.cs
+++ Runtime/Scripts/UI/Popup/PopupWindow.cs
@@ -1,10 +1,21 @@
 ﻿using enp_unity_extensions.Scripts.UI.Windows;
+using UnityEngine;
 using static enp_unity_extensions.Scripts.UI.Windows.AnimatedWindowConstant;
 
 namespace enp_unity_extensions.Runtime.Scripts.UI.Popup
 {
     public class PopupWindow : AnimatedWindow
     {
+        private void OnValidate()
+        {
+            var className = nameof(PopupWindow);
+
+            if (gameObject.name != className)
+            {
+                gameObject.name = className;
+            }
+        }
+
         public void Close(AnimatedWindowConstant closeAnim = CloseMiddle)
         {
             PopupController.Close(this, closeAnim);
