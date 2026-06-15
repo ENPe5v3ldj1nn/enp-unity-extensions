433796d8ca94e7354ebf7c7f3eff286ce49db993
Dmytro <raijlink@gmail.com>
2026-06-15 19:23:40 +0300

    update button

--- Runtime/Scripts/UI/Button/AnimatedButton.cs
+++ Runtime/Scripts/UI/Button/AnimatedButton.cs
@@ -1,45 +1,109 @@
-using System.Collections;
+using System;
 using TMPro;
-using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 
 namespace ENP.UnityExtensions.Runtime.Scripts.UI.Button
 {
-    public class AnimatedButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
+    [AddComponentMenu("UI/Animated Button")]
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(CanvasGroup))]
+    [RequireComponent(typeof(RectTransform))]
+    public sealed class AnimatedButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
     {
-        [SerializeField] private float _animValue = 0.95f;
-        public TMP_Text text;
-        public CanvasGroup _canvasGroup;
-        public RectTransform _rectTransform;
-        public bool Interactable = true;
-        public bool IsUseAnimation = true;
-        private readonly UnityEvent _onClick = new();
-        private bool _blockInput;
-        private static WaitForSeconds _blockInputDelay = new WaitForSeconds(0.25f);
+        [Header("Visual")]
+        [FormerlySerializedAs("IsUseAnimation")]
+        [SerializeField] private bool _useAnimation = true;
+        [FormerlySerializedAs("_animValue")]
+        [SerializeField, Range(0.01f, 1f)] private float _pressedScale = 0.95f;
+        [SerializeField, Min(0f)] private float _animationDuration = 0.2f;
+        [FormerlySerializedAs("Interactable")]
+        [SerializeField] private bool _interactable = true;
+        [SerializeField, Range(0f, 1f)] private float _disabledAlpha = 0.6f;
 
-        private void OnValidate()
+        [Header("Input")]
+        [SerializeField, Min(0f)] private float _clickBlockDuration = 0.25f;
+
+        [Header("Optional")]
+        [FormerlySerializedAs("text")]
+        [SerializeField] private TMP_Text _text;
+
+        [Header("Events")]
+        [SerializeField] private UnityEvent _onClick = new();
+
+        private CanvasGroup _canvasGroup;
+        private bool _isPointerDown;
+        private float _blockInputUntilTime;
+
+        public event Action Pressed;
+        public event Action Released;
+
+        public bool Interactable
         {
-            if (TryGetComponent<RectTransform>(out var component))
-            {
-                _rectTransform = component;
-            }
+            get => _interactable;
+            set => SetInteractable(value);
+        }
 
-            if (TryGetComponent<CanvasGroup>(out var canvasGroup))
+        public bool UseAnimation
+        {
+            get => _useAnimation;
+            set => _useAnimation = value;
+        }
+
+        public float PressedScale
+        {
+            get => _pressedScale;
+            set => _pressedScale = value;
+        }
+
+        public float AnimationDuration
+        {
+            get => _animationDuration;
+            set => _animationDuration = value;
+        }
+
+        public float DisabledAlpha
+        {
+            get => _disabledAlpha;
+            set
             {
-                _canvasGroup = canvasGroup;
+                _disabledAlpha = value;
+                ApplyInteractableVisual();
             }
+        }
+
+        public float ClickBlockDuration
+        {
+            get => _clickBlockDuration;
+            set => _clickBlockDuration = value;
+        }
+
+        public TMP_Text Text => _text;
+
+        private void Awake()
+        {
+            CacheReferences();
+            ApplyInteractableVisual();
         }
 
         private void OnEnable()
         {
-            _blockInput = false;
+            _isPointerDown = false;
+            _blockInputUntilTime = 0f;
+            ApplyInteractableVisual();
+        }
+
+        private void OnValidate()
+        {
+            CacheReferences();
+            ApplyInteractableVisual();
         }
 
         public void AddListener(UnityAction onClick)
         {
-            _onClick.AddListener(onClick.Invoke);
+            _onClick.AddListener(onClick);
         }
 
         public void RemoveListener()
@@ -47,77 +111,94 @@ namespace ENP.UnityExtensions.Runtime.Scripts.UI.Button
             _onClick.RemoveAllListeners();
         }
 
+        public void RemoveListener(UnityAction onClick)
+        {
+            _onClick.RemoveListener(onClick);
+        }
+
         public void ForceInvoke()
         {
             _onClick.Invoke();
         }
 
-        private void Press()
-        {
-            _onClick.Invoke();
-        }
-
-
-        private void BlockInputTemporarily()
-        {
-            BlockInputTemp().Start();
-        }
-
-        IEnumerator BlockInputTemp()
-        {
-            _blockInput = true;
-            yield return _blockInputDelay;
-            _blockInput = false;
-        }
-
-        private bool CanClick(PointerEventData eventData)
-        {
-            if (eventData.button != PointerEventData.InputButton.Left || !Interactable)
-            {
-                return false;
-            }
-
-            if (_blockInput)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
         public void OnPointerDown(PointerEventData eventData)
         {
-            if (!CanClick(eventData)) return;
-
-            if (IsUseAnimation)
+            if (!CanInteract(eventData))
             {
-                transform.DOScale(Vector3.one * _animValue, 0.2f);
+                return;
             }
-            else
-            {
-                transform.localScale = Vector3.one * _animValue;
-            }
-        }
 
-        public void OnPointerClick(PointerEventData eventData)
-        {
-            if (!CanClick(eventData)) return;
-
-            Press();
-            BlockInputTemporarily();
+            _isPointerDown = true;
+            Pressed?.Invoke();
         }
 
         public void OnPointerUp(PointerEventData eventData)
         {
-            if (IsUseAnimation)
+            if (eventData.button != PointerEventData.InputButton.Left || !_isPointerDown)
             {
-                transform.DOScale(Vector3.one, 0.2f);
-            }
-            else
-            {
-                transform.localScale = Vector3.one;
+                return;
             }
 
+            _isPointerDown = false;
+            Released?.Invoke();
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (!CanClick(eventData))
+            {
+                return;
+            }
+
+            _onClick.Invoke();
+            _blockInputUntilTime = Time.unscaledTime + _clickBlockDuration;
+        }
+
+        public void SetInteractable(bool value)
+        {
+            if (_interactable == value)
+            {
+                ApplyInteractableVisual();
+                return;
+            }
+
+            _interactable = value;
+
+            if (!_interactable && _isPointerDown)
+            {
+                _isPointerDown = false;
+                Released?.Invoke();
+            }
+
+            ApplyInteractableVisual();
+        }
+
+        private void CacheReferences()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+            Debug.Assert(_canvasGroup != null, "AnimatedButton requires a CanvasGroup.");
+        }
+
+        private bool CanInteract(PointerEventData eventData)
+        {
+            return eventData.button == PointerEventData.InputButton.Left && _interactable && !IsInputBlocked();
+        }
+
+        private bool CanClick(PointerEventData eventData)
+        {
+            return eventData.button == PointerEventData.InputButton.Left && _interactable && !IsInputBlocked();
+        }
+
+        private bool IsInputBlocked()
+        {
+            return Time.unscaledTime < _blockInputUntilTime;
+        }
+
+        private void ApplyInteractableVisual()
+        {
+            _canvasGroup.interactable = _interactable;
+            _canvasGroup.blocksRaycasts = _interactable;
+            _canvasGroup.alpha = _interactable ? 1f : Mathf.Clamp01(_disabledAlpha);
         }
     }
 }
---
+++ Runtime/Scripts/UI/Button/AnimatedButtonAnimation.cs
@@ -0,0 +1,90 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace ENP.UnityExtensions.Runtime.Scripts.UI.Button
+{
+    [AddComponentMenu("UI/Animated Button Animation")]
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(AnimatedButton))]
+    [RequireComponent(typeof(RectTransform))]
+    public sealed class AnimatedButtonAnimation : MonoBehaviour
+    {
+        private AnimatedButton _button;
+        private RectTransform _rectTransform;
+        private Tween _scaleTween;
+        private Vector3 _restScale;
+
+        private void Awake()
+        {
+            CacheReferences();
+        }
+
+        private void OnEnable()
+        {
+            CacheReferences();
+            _restScale = _rectTransform.localScale;
+            _button.Pressed += HandlePressed;
+            _button.Released += HandleReleased;
+        }
+
+        private void OnDisable()
+        {
+            _button.Pressed -= HandlePressed;
+            _button.Released -= HandleReleased;
+            ResetScale();
+        }
+
+        private void OnValidate()
+        {
+            CacheReferences();
+        }
+
+        private void HandlePressed()
+        {
+            SetScale(_restScale * _button.PressedScale);
+        }
+
+        private void HandleReleased()
+        {
+            SetScale(_restScale);
+        }
+
+        private void SetScale(Vector3 targetScale)
+        {
+            KillTween();
+
+            if (_button.UseAnimation)
+            {
+                _scaleTween = _rectTransform.DOScale(targetScale, _button.AnimationDuration);
+                return;
+            }
+
+            _rectTransform.localScale = targetScale;
+        }
+
+        private void ResetScale()
+        {
+            KillTween();
+            _rectTransform.localScale = _restScale;
+        }
+
+        private void KillTween()
+        {
+            if (_scaleTween == null)
+            {
+                return;
+            }
+
+            _scaleTween.Kill();
+            _scaleTween = null;
+        }
+
+        private void CacheReferences()
+        {
+            _button = GetComponent<AnimatedButton>();
+            _rectTransform = GetComponent<RectTransform>();
+            Debug.Assert(_button != null, "AnimatedButtonAnimation requires an AnimatedButton.");
+            Debug.Assert(_rectTransform != null, "AnimatedButtonAnimation requires a RectTransform.");
+        }
+    }
+}
